1cb2fecef9d7420f7aad65a66c54bd6be70e4049
ToddLandis <toddlandis@cedarville.edu>
2022-01-26 17:12:17 -0500

    Pushing from John

---
+++ gitupdate.sh
@@ -0,0 +1,6 @@
+echo Pushing to Github
+git push git@github.com:ToddLandis/cs3220.git
+echo Pulling from Github
+git pull git@github.com:ToddLandis/cs3220.git
+echo Setting files to be executable
+chmod 755 Project1/*
