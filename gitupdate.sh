b39a3518e71e8984c19e7c37b69114c37c149c7a
ToddLandis <toddlandis@cedarville.edu>
2022-01-26 17:17:59 -0500

    Pushing from John

--- gitupdate.sh
+++ gitupdate.sh
@@ -1,3 +1,7 @@
+echo Adding Changes
+git add --all
+echo Committing Changes
+git commit -m "Pushing from John"
 echo Pushing to Github
 git push git@github.com:ToddLandis/cs3220.git
 echo Pulling from Github
