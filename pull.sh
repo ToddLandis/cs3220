8bd117a17ec0684ce6ede63020cc491191c4a0a1
ToddLandis <toddlandis@cedarville.edu>
2022-01-27 19:35:38 -0500

    Pushing from John

--- pull.sh
+++ pull.sh
@@ -1,4 +1,5 @@
 echo "\nPulling from Github"
+git fetch git@github.com:ToddLandis/cs3220.git
 git pull git@github.com:ToddLandis/cs3220.git
 echo "\nSetting files to be executable"
 chmod -R 755 Project1 Project2
