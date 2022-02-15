88fff5f8265a280202779779a88d18c0138074ea
Todd Landis <73204491+ToddLandis@users.noreply.github.com>
2022-02-14 22:06:01 -0500

    Create script.js
    Project 2 javascript

---
+++ Project2/script.js
@@ -0,0 +1,87 @@
+class Course {
+	constructor(term, year, courseDesignator, courseName) {
+		this.term = term;
+		this.year = year;
+		this.courseDesignator = courseDesignator;
+		this.courseName = courseName;
+	}
+}
+class Plan {
+	constructor(planName, catalogueYear, major, studentName, semester) {
+		this.planName = planName;
+		this.catalogueYear = catalogueYear;
+		this.major = major;
+		this.studentName = studentName;
+		this.semester = semester;
+		this.courses = [];
+	}
+	function addCourse(course) {
+		this.courses.push(course);
+	}
+}
+
+class Year {
+	constructor() {
+		this.terms = {};
+	}
+	function addTerm(term) {
+		this.terms.push(term);
+	}
+}
+
+class Term {
+	constructor() {
+		this.courses = [];
+	}
+	function addCourse(course) {
+		this.courses.push(course);
+	}
+}
+
+function populate() {
+	var myPlan = new Plan("myPlan", "2022", "Comp. Sci.", "Fredrick Douglas", "Fall");
+	
+	myPlan.addCourse(new Course("Fall", "2022", "CS-1210", "C++ Programming"));
+	myPlan.addCourse(new Course("Fall", "2022", "EG-1210", "Digital Logic Design"));
+	myPlan.addCourse(new Course("Spring", "2023", "CS-1220", "C++ Programming 2"));
+	myPlan.addCourse(new Course("Spring", "2023", "CS-1230", "Python Programming"));
+	myPlan.addCourse(new Course("Summer", "2023", "Math-100", "Remediadiary Math"));
+	myPlan.addCourse(new Course("Summer", "2023", "Eng-100", "Remediadiary Reading"));
+	myPlan.addCourse(new Course("Fall", "2023", "CS-1240", "Java Programming"));
+	myPlan.addCourse(new Course("Fall", "2023", "CS-1250", "Dominating the World"));
+	
+	myPlan.addCourse(new Course("Fall", "2023", "CS-1210", "C++ Programming"));
+	myPlan.addCourse(new Course("Fall", "2023", "EG-1210", "Digital Logic Design"));
+	myPlan.addCourse(new Course("Spring", "2024", "CS-1220", "C++ Programming 2"));
+	myPlan.addCourse(new Course("Spring", "2024", "CS-1230", "Python Programming"));
+	myPlan.addCourse(new Course("Summer", "2024", "Math-100", "Remediadiary Math"));
+	myPlan.addCourse(new Course("Summer", "2024", "Eng-100", "Remediadiary Reading"));
+	myPlan.addCourse(new Course("Fall", "2024", "CS-1240", "Java Programming"));
+	myPlan.addCourse(new Course("Fall", "2024", "CS-1250", "Dominating the World"));
+	
+	myPlan.addCourse(new Course("Fall", "2024", "CS-1210", "C++ Programming"));
+	myPlan.addCourse(new Course("Fall", "2024", "EG-1210", "Digital Logic Design"));
+	myPlan.addCourse(new Course("Spring", "2025", "CS-1220", "C++ Programming 2"));
+	myPlan.addCourse(new Course("Spring", "2025", "CS-1230", "Python Programming"));
+	myPlan.addCourse(new Course("Summer", "2025", "Math-100", "Remediadiary Math"));
+	myPlan.addCourse(new Course("Summer", "2025", "Eng-100", "Remediadiary Reading"));
+	myPlan.addCourse(new Course("Fall", "2025", "CS-1240", "Java Programming"));
+	myPlan.addCourse(new Course("Fall", "2025", "CS-1250", "Dominating the World"));
+	
+	myPlan.addCourse(new Course("Fall", "2025", "CS-1210", "C++ Programming"));
+	myPlan.addCourse(new Course("Fall", "2025", "EG-1210", "Digital Logic Design"));
+	myPlan.addCourse(new Course("Spring", "2026", "CS-1220", "C++ Programming 2"));
+	myPlan.addCourse(new Course("Spring", "2026", "CS-1230", "Python Programming"));
+	myPlan.addCourse(new Course("Summer", "2026", "Math-100", "Remediadiary Math"));
+	myPlan.addCourse(new Course("Summer", "2026", "Eng-100", "Remediadiary Reading"));
+	myPlan.addCourse(new Course("Fall", "2026", "CS-1240", "Java Programming"));
+	myPlan.addCourse(new Course("Fall", "2026", "CS-1250", "Dominating the World"));
+}
+
+function convert(plan) {
+	
+	var myYears = {};
+	for (i in plan.courses) {
+		myYears[i.year][i.term].push(i);
+	}
+}
