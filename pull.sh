1ff94bfede2e67b64f58cdf2f836f253162889ee
ToddLandis <toddlandis@cedarville.edu>
2022-02-21 17:22:52 -0500

    Pushing from John

--- pull.sh
+++ pull.sh
@@ -1,6 +1,6 @@
 echo "\nPulling from Github"
 git pull git@github.com:ToddLandis/cs3220.git
 echo "\nSetting files to be executable"
-chmod -R 755 Project1 Project2
+chmod -R 755 Project{1..6}
 echo "\nPushing possible changes (file permissions) back to GitHub"
 push.sh
