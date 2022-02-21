be49ade5e342c7246efea7974788f2cfe573b4ec
joshyhearne <81659779+joshyhearne@users.noreply.github.com>
2022-02-21 17:36:48 -0500

    Added hours

--- Project3/script.js
+++ Project3/script.js
@@ -1,136 +1,3 @@
-class Course {
-	constructor(term, year, courseDesignator, courseName) {
-		this.term = term;
-		this.year = year;
-		this.courseDesignator = courseDesignator;
-		this.courseName = courseName;
-	}
-}
-
-class Plan {
-	constructor(planName, catalogueYear, major, studentName, semester, courses = []) {
-		this.planName = planName;
-		this.catalogueYear = catalogueYear;
-		this.major = major;
-		this.studentName = studentName;
-		this.semester = semester;
-		this.courses = courses;
-	}
-	addCourse(course) {
-		this.courses.push(course);
-	}
-}
-
-class Year {
-	constructor() {
-		this.terms = {};
-	}
-	addTerm(key, term) {
-		this.terms[key] = term;
-	}
-}
-
-class Term {
-	constructor(courses = []) {
-		this.courses = courses; //FIXME I think the issue might be with the array declarations
-	}
-	addCourse(course) {
-		this.courses.push(course);
-	}
-}
-
-function populate() {
-	var myPlan = new Plan("myPlan", "2022", "Comp. Sci.", "Fredrick Douglas", "Fall");
-	
-	myPlan.addCourse(new Course("Fall", "2022", "CS-1210", "C++ Programming"));
-	myPlan.addCourse(new Course("Fall", "2022", "EG-1210", "Digital Logic Design"));
-	myPlan.addCourse(new Course("Spring", "2023", "CS-1220", "C++ Programming 2"));
-	myPlan.addCourse(new Course("Spring", "2023", "CS-1230", "Python Programming"));
-	myPlan.addCourse(new Course("Summer", "2023", "Math-100", "Remedial Math"));
-	myPlan.addCourse(new Course("Summer", "2023", "Eng-100", "Remedial Reading"));
-	
-	myPlan.addCourse(new Course("Fall", "2023", "CS-1210", "C++ Programming"));
-	myPlan.addCourse(new Course("Fall", "2023", "EG-1210", "Digital Logic Design"));
-	myPlan.addCourse(new Course("Spring", "2024", "CS-1220", "C++ Programming 2"));
-	myPlan.addCourse(new Course("Spring", "2024", "CS-1230", "Python Programming"));
-	myPlan.addCourse(new Course("Summer", "2024", "Math-100", "Remedial Math"));
-	myPlan.addCourse(new Course("Summer", "2024", "Eng-100", "Remedial Reading"));
-	
-	myPlan.addCourse(new Course("Fall", "2024", "CS-1210", "C++ Programming"));
-	myPlan.addCourse(new Course("Fall", "2024", "EG-1210", "Digital Logic Design"));
-	myPlan.addCourse(new Course("Spring", "2025", "CS-1220", "C++ Programming 2"));
-	myPlan.addCourse(new Course("Spring", "2025", "CS-1230", "Python Programming"));
-	myPlan.addCourse(new Course("Summer", "2025", "Math-100", "Remedial Math"));
-	myPlan.addCourse(new Course("Summer", "2025", "Eng-100", "Remedial Reading"));
-	
-	myPlan.addCourse(new Course("Fall", "2025", "CS-1210", "C++ Programming"));
-	myPlan.addCourse(new Course("Fall", "2025", "EG-1210", "Digital Logic Design"));
-	myPlan.addCourse(new Course("Spring", "2026", "CS-1220", "C++ Programming 2"));
-	myPlan.addCourse(new Course("Spring", "2026", "CS-1230", "Python Programming"));
-	myPlan.addCourse(new Course("Summer", "2026", "Math-100", "Remedial Math"));
-	myPlan.addCourse(new Course("Summer", "2026", "Eng-100", "Remedial Reading"));
-	
-	return myPlan;
-}
-
-function convert(plan) {
-	
-	var myYears = {};
-	courses = plan.courses;
-	for (let i in courses) {
-		if (courses[i].term == "Fall") {
-			year = courses[i].year
-			if (!(year in myYears)) {
-				myYears[courses[i].year] = new Year();
-			}
-			if (!(courses[i].term in myYears[courses[i].year].terms)) {
-				myYears[courses[i].year].addTerm("Fall", new Term());
-				myYears[courses[i].year].addTerm("Spring", new Term());
-				myYears[courses[i].year].addTerm("Summer", new Term());
-			}
-		}
-		if (courses[i] !== undefined) {
-			myYears[year].terms[courses[i].term].addCourse(courses[i]);
-		}
-	}
-	return myYears;
-}
-
-function make() {
-	var years = convert(populate());
-	var instring = "";
-	
-	for (let year in years) {
-		thisYear = years[year];
-		instring = instring.concat("<div class=\"years\">");
-		
-		for (let term in thisYear.terms) {
-			thisTerm = thisYear.terms[term];
-			
-			if (term == "Fall") {
-				instring = instring.concat("<div class=\"semester\"><h4>" + term + " " + year + "</h4>");
-			} else {
-				instring = instring.concat("<div class=\"semester\"><h4>" + term + " " + (parseInt(year)+1).toString() + "</h4>");
-			}
-			
-			for (let course in thisTerm.courses) {
-				thisCourse = thisTerm.courses[course];
-				instring = instring.concat("<p>" + thisCourse.courseName + "</p>");
-			}
-			
-			instring = instring.concat("</div>");
-		}
-		
-		instring = instring.concat("</div>");
-	}
-	
-	doc = document.getElementById("UR");
-	doc.innerHTML = instring;
-}
-
-make();
-
-
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function dropdownFunction() {
@@ -150,3 +17,157 @@ function dropdownFunction() {
       }
     }
   }
+
+  class Course {
+    constructor(term, year, courseDesignator, courseName, hours) {
+      this.term = term;
+      this.year = year;
+      this.courseDesignator = courseDesignator;
+      this.courseName = courseName;
+      this.hours = hours;
+    }
+  }
+  
+  class Plan {
+    constructor(planName, catalogueYear, major, studentName, semester, courses = []) {
+      this.planName = planName;
+      this.catalogueYear = catalogueYear;
+      this.major = major;
+      this.studentName = studentName;
+      this.semester = semester;
+      this.courses = courses;
+    }
+    addCourse(course) {
+      this.courses.push(course);
+    }
+  }
+  
+  class Year {
+    constructor() {
+      this.terms = {};
+    }
+    addTerm(key, term) {
+      this.terms[key] = term;
+    }
+  }
+  
+  class Term {
+    constructor(courses = []) {
+      this.courses = courses;
+      this.hours = 0;
+    }
+    addCourse(course) {
+      this.courses.push(course);
+    }
+  }
+  
+  function populate() {
+    var myPlan = new Plan("myPlan", "2022", "Comp. Sci.", "Fredrick Douglas", "Fall");
+    
+    myPlan.addCourse(new Course("Summer", "2023", "Eng-100", "Remediadiary Reading", 1));
+    myPlan.addCourse(new Course("Fall", "2022", "CS-1210", "C++ Programming", 3));
+    myPlan.addCourse(new Course("Fall", "2022", "EG-1210", "Digital Logic Design", 3));
+    myPlan.addCourse(new Course("Spring", "2023", "CS-1220", "C++ Programming 2", 3));
+    myPlan.addCourse(new Course("Spring", "2023", "CS-1230", "Python Programming", 3));
+    myPlan.addCourse(new Course("Spring", "2023", "OK-1010", "Intro to Introductions", 3));
+    myPlan.addCourse(new Course("Spring", "2023", "CS-6590", "Python Programming for losers", 3));
+    myPlan.addCourse(new Course("Summer", "2023", "Math-100", "Remediadiary Math", 2));
+    
+    myPlan.addCourse(new Course("Fall", "2023", "CS-1210", "C++ Programming", 3));
+    myPlan.addCourse(new Course("Fall", "2023", "EG-1210", "Digital Logic Design", 3));
+    myPlan.addCourse(new Course("Spring", "2024", "CS-1220", "C++ Programming 2", 3));
+    myPlan.addCourse(new Course("Spring", "2024", "CS-1230", "Python Programming", 3));
+    myPlan.addCourse(new Course("Summer", "2024", "Math-100", "Remediadiary Math", 2));
+    myPlan.addCourse(new Course("Summer", "2024", "Eng-100", "Remediadiary Reading", 1));
+    
+    myPlan.addCourse(new Course("Fall", "2024", "CS-1210", "C++ Programming", 3));
+    myPlan.addCourse(new Course("Fall", "2024", "EG-1210", "Digital Logic Design", 3));
+    myPlan.addCourse(new Course("Spring", "2025", "CS-1220", "C++ Programming 2", 3));
+    myPlan.addCourse(new Course("Spring", "2025", "CS-1230", "Python Programming", 3));
+    myPlan.addCourse(new Course("Summer", "2025", "Math-100", "Remediadiary Math", 2));
+    myPlan.addCourse(new Course("Summer", "2025", "Eng-100", "Remediadiary Reading", 1));
+    
+    myPlan.addCourse(new Course("Fall", "2025", "CS-1210", "C++ Programming", 3));
+    myPlan.addCourse(new Course("Fall", "2025", "EG-1210", "Digital Logic Design", 3));
+    myPlan.addCourse(new Course("Spring", "2026", "CS-1220", "C++ Programming 2", 3));
+    myPlan.addCourse(new Course("Spring", "2026", "CS-1230", "Python Programming", 3));
+    myPlan.addCourse(new Course("Summer", "2026", "Math-100", "Remediadiary Math", 2));
+    myPlan.addCourse(new Course("Summer", "2026", "Eng-100", "Remediadiary Reading", 1));
+    
+    return myPlan;
+  }
+  
+  function convert(plan) {
+    
+    var myYears = {};
+    
+    courses = plan.courses;
+    for (let i in courses) {
+      
+      // Year object holds Fall of same year, but Spring and Summer of next year
+      if (courses[i].term == "Fall") {
+        year = courses[i].year
+      }
+      else {
+        year = (parseInt(courses[i].year) - 1).toString();
+      }
+      // if the year does not yet exist, make it
+      if (!(year in myYears)) {
+        myYears[year] = new Year();
+      }
+      // if the term does not yet exist, make it
+      if (!(courses[i].term in myYears[year].terms)) {
+        myYears[year].addTerm("Fall", new Term());
+        myYears[year].addTerm("Spring", new Term());
+        myYears[year].addTerm("Summer", new Term());
+      }
+      if (courses[i] !== undefined) {
+        myYears[year].terms[courses[i].term].addCourse(courses[i]);
+      }
+    }
+    return myYears;
+  }
+  
+  function make() {
+    var years = convert(populate());
+    var instring = "";
+    
+    instring = instring.concat("<div class=\"profile\"></div>")
+
+    for (let year in years) {
+      thisYear = years[year];
+      instring = instring.concat("<div class=\"years\">");
+      
+      for (let term in thisYear.terms) {
+        thisTerm = thisYear.terms[term];
+        
+        // add header to the term
+        if (term == "Fall") {
+          instring = instring.concat("<div class=\"semester\"><div class=\"term_head\"><h4>" + term + " " + year + "</h4>");
+        } else {
+          instring = instring.concat("<div class=\"semester\"><div class=\"term_head\"><h4>" + term + " " + (parseInt(year)+1).toString() + "</h4>");
+        }
+
+        // add hours to each term
+        var hours = 0
+        for (let course in thisTerm.courses) {
+          hours += thisTerm.courses[course].hours;
+        }
+        instring = instring.concat("<h5>Hours: " + hours + "</h5></div>")
+        
+        // add each course to the term
+        for (let course in thisTerm.courses) {
+          thisCourse = thisTerm.courses[course];
+          instring = instring.concat("<p onfocus=\"\">" + thisCourse.courseDesignator + ":  " + thisCourse.courseName + "</p>");
+        }
+
+        instring = instring.concat("</div>");
+      }
+      instring = instring.concat("</div>");
+    }
+    // set #UR to be dynamic
+    doc = document.getElementById("UR");
+    doc.innerHTML = instring;
+  }
+  
+  make();
