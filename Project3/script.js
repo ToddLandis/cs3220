34a6cf6c817faba9d740bd9518d73cb59c04f8cc
Todd Landis <73204491+ToddLandis@users.noreply.github.com>
2022-02-22 11:05:51 -0500

    update populate()

--- Project3/script.js
+++ Project3/script.js
@@ -64,7 +64,7 @@ function dropdownFunction() {
   function populate() {
     var request = new XMLHttpRequest();
 	  request.open("GET","/~gallaghd/cs3220/termProject/getCombined.php",true);
-	  request.onreadystatechange,response = function() {
+	  response,request.onreadystatechange = function() {
 		if(this.readyState == 4) {
 			var myData = request.responseText;
       return myData
