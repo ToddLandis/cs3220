c1889446482b32c44957af5f43375bb0ef30530a
joshyhearne <81659779+joshyhearne@users.noreply.github.com>
2022-02-23 18:40:49 -0500

    fixed navbar buttons

--- Project3/script.js
+++ Project3/script.js
@@ -1,9 +1,13 @@
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
-function dropdownFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+function projDropdownFunction() {
+  document.getElementById("projDropdown").classList.toggle("show");
 }
-  
+
+function linksDropdownFunction() {
+  document.getElementById("linksDropdown").classList.toggle("show");
+}
+
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
