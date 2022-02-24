f5cb2093fc026f6df6a8f206236bcf55927701ae
Todd Landis <73204491+ToddLandis@users.noreply.github.com>
2022-02-23 21:47:23 -0500

    weird

--- Project3/script.js
+++ Project3/script.js
@@ -200,10 +200,6 @@ function make(years) {
   doc.innerHTML = instring;
 }
   
-// make(); // had to change with async
-populate(); // main async function
-// Anything that uses the AJAX data should be called in populate() at the location marked with a comment //AJAXfunctions
-
 
 // Kelly Blue Book Problem //
 
@@ -225,3 +221,11 @@ request.onreadystatechange = function() {
     html.innerHTML = string;
   }
 }
+
+
+
+// make(); // had to change with async
+populate(); // main async function
+// Anything that uses the AJAX data should be called in populate() at the location marked with a comment //AJAXfunctions
+
+
