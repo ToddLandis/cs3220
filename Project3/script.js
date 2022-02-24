043b4015240fc347d13d3f9ebc81315781c3f28e
joshyhearne <81659779+joshyhearne@users.noreply.github.com>
2022-02-23 21:10:09 -0500

    added style.js

--- Project3/script.js
+++ Project3/script.js
@@ -1,31 +1,4 @@
 // Include Scripts
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function dropdownFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-  
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
-
-/* dark mode function */
-function darkModeFunction() {
-  var element = document.body;
-  var content = document.getElementsByClassName("modebtn");
-  element.classList.toggle("dark-mode");
-  content.innerText = "Light Mode";
-}
 
 class Course {
   constructor(term, year, courseDesignator, courseName, hours) {
