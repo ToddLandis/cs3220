1a4f0e4ac2292a0aff18ca01b9af9087bb40a66d
Todd Landis <73204491+ToddLandis@users.noreply.github.com>
2022-03-20 16:28:31 -0400

    fix query

--- Project4/getAll.php
+++ Project4/getAll.php
@@ -1,6 +1,6 @@
 <?php
     $db = mysqli_connect("james","cs3220","","cs3220_Sp22") or die ("Error: Unable to connected to database");
-    $query = "SELECT * from CHL_Users";
+    $query = "SELECT * FROM `CHL_User` WHERE 1";
     $result = mysqli_query($db, $query) or die ("Error: Unsuccessful query");
 
     // $row = mysqli_fetch_assoc($result); // Hash where values can be accessed using column names
