cb0471888845083f68d0e4a4b1cf076f0cfd82b1
Todd Landis <73204491+ToddLandis@users.noreply.github.com>
2022-03-20 16:29:59 -0400

    add \n

--- Project4/getAll.php
+++ Project4/getAll.php
@@ -4,7 +4,7 @@
     $result = mysqli_query($db, $query) or die ("Error: Unsuccessful query");
 
     // $row = mysqli_fetch_assoc($result); // Hash where values can be accessed using column names
-    print "Student Names";
+    print "Student Names\n";
     for ($rowNum = 0; $rowNum < mysqli_num_rows($result); $rowNum++) {
         $row = mysqli_fetch_assoc($result);
         print htmlspecialchars($row["Name"]);
