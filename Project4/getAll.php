fea2ad7d37a2c66a9c822d5937ac8bdc16f3402d
Todd Landis <73204491+ToddLandis@users.noreply.github.com>
2022-03-21 21:27:02 -0400

    test

--- Project4/getAll.php
+++ Project4/getAll.php
@@ -29,7 +29,26 @@ $stmt->bind_result($fieldA, $fieldB, $fieldC);
 
 $user = array();
 while ($stmt->fetch()) { //FIXME shouldn't need loop in this instance
-    array_push($user, $fieldA, $fieldB, $fieldC);
+    array_push($user, "ID"=>$fieldA, "Name"=>$fieldB, "Dark_Mode"=>$fieldC);
+}
+$stmt->close();
+
+
+$stmt = $mysqli->prepare("SELECT ID, User_ID, Catalog_ID, Plan_Name from CHL_Plan
+        where User_ID = ?");
+$stmt->bind_param("i", $ID)
+        or die('Database bind error.');
+
+$stmt->execute()
+        or die('Database execute error.');
+$stmt->store_result();    // optional for efficiency; fetches all results
+$stmt->bind_result($fieldA, $fieldB, $fieldC, $fieldD);
+
+$plan = array();
+$i = 0;
+while ($stmt->fetch()) {
+    $plan[i] = ["ID"=>$fieldA, "User_ID"=>$fieldB, "Catalog_ID"=>$fieldC, Plan_Name=>$fieldD];
+    $i++;
 }
 $stmt->close();
 
@@ -49,6 +68,7 @@ while ($stmt->fetch()) {
     array_push($plan, $fieldA, $fieldB, $fieldC, $fieldD);
 }
 $stmt->close();
+
 
 // top level
 $combined['plan'] = $plan;
