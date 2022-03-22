0fccf27e760b9abc0fa4048fa5a9fc9d1a146917
joshyhearne <81659779+joshyhearne@users.noreply.github.com>
2022-03-22 14:43:09 -0400

    styles/sessions

--- Project4/login.php
+++ Project4/login.php
@@ -15,14 +15,16 @@
                     <p class="credits">By Joshua Hearne, Todd Landis, and Ryan Carpenter</p>
                 </div>
             </div>
-            <div id="login-container">
-                <form name="loginform" onsubmit="validate()">
-                    <input id="userfield" type="text" placeholder="Username"/>
-                    <input id="passfield" type="password" placeholder="Password"/>
-                    <input id="submit-btn" type="submit" value="Log in"/>
-                </form>
-                <p id="or">or</p>
-                <a href="./signup.html" id="signup">Sign up</a>
+            <div id="content-wrap">
+                <div id="login-container">
+                    <form name="loginform" onsubmit="validate()">
+                        <input id="userfield" type="text" placeholder="Username"/>
+                        <input id="passfield" type="password" placeholder="Password"/>
+                        <input id="submit-btn" type="submit" value="Log in"/>
+                    </form>
+                    <p id="or">or</p>
+                    <a href="./signup.html" id="signup">Sign up</a>
+                </div>
             </div>
         </div>
     </body>
