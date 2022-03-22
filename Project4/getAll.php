44ad99a2e89f014a3f2cb4564b067506f9fff1b6
Todd Landis <73204491+ToddLandis@users.noreply.github.com>
2022-03-22 00:39:05 -0400

    php

--- Project4/getAll.php
+++ Project4/getAll.php
@@ -20,7 +20,7 @@ $major=1;  // id field for CS major; get from Plan FIXME
 $stmt = $mysqli->prepare("SELECT ID, Name, Dark_Mode from CHL_User 
         where ID = ?")
         or die("Prepare error.");
-$stmt->bind_param("i", $ID) //FIXME probably remove "ii"
+$stmt->bind_param("1", $ID) //FIXME probably remove "ii"
         or die('Database bind error.');
 
 $stmt->execute()
