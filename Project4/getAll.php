8dc757487990e1b82de11010a5a74a2fbe252974
Todd Landis <73204491+ToddLandis@users.noreply.github.com>
2022-03-21 20:46:06 -0400

    php progress

--- Project4/getAll.php
+++ Project4/getAll.php
@@ -3,11 +3,50 @@ $mysqli = new mysqli('james', 'cs3220', '', 'cs3220_Sp22')
     or die('Database connect error.');
 
 // Arguments
-$name = htmlspecialchars($GET["name"]);
+$ID = htmlspecialchars($GET["ID"]);
 $password = htmlspecialchars($GET["pass"]); //FIXME session key
 
 // Return array
 $combined = array();
+
+
+
+$year = 2018;   // get from Plan
+$major=1;  // id field for CS major; get from Plan
+
+
+$stmt = mysqli->prepare("SELECT ID, Name, Dark_Mode from CHL_User 
+        where ID = ?");
+$stmt->bind_param("ii", $ID)
+        or die('Database bind error.');
+
+$stmt->execute()
+        or die('Database execute error.');
+$stmt->store_result();    // optional for efficiency; fetches all results
+$stmt->bind_result($fieldA, $fieldB, $fieldC);
+
+$user = array();
+while ($stmt->fetch()) { //FIXME shouldn't need loop in this instance
+    array_push($user, $fieldA, $fieldB, $fieldC);
+}
+$stmt->close();
+
+
+$stmt = mysqli->prepare("SELECT ID, User_ID, Catalog_ID, Plan_Name from CHL_Plan
+        where User_ID = ?");
+$stmt->bind_param("ii", $ID)
+        or die('Database bind error.');
+
+$stmt->execute()
+        or die('Database execute error.');
+$stmt->store_result();    // optional for efficiency; fetches all results
+$stmt->bind_result($fieldA, $fieldB, $fieldC, $fieldD);
+
+$plan = array();
+while ($stmt->fetch()) {
+    array_push($plan, $fieldA, $fieldB, $fieldC, $fieldD);
+}
+$stmt->close();
 
 // top level
 $combined['plan'] = $plan;
@@ -15,27 +54,6 @@ $combined['catalog'] = $catalog;
 $combined['Requirements'] = $requirements;
 $combined['User'] = $user;
 $combined['PlanList'] = $planList;
-
-$year = 2018;   // get from Plan
-$major=1;  // id field for CS major; get from Plan
-
-$stmt = mysqli->prepare("SELECT fieldA, fieldB from Requirements 
-        where year= ? AND  major= ?;");
-$stmt->bind_param("ii", $year, $major)
-        or die('Database bind error.');
-
-$stmt->execute()
-        or die('Database execute error.');
-$stmt->store_result();    // optional for efficiency; fetches all results
-$stmt->bind_result($fieldA, $fieldB);
-
-$reqs = array();
-while ($stmt->fetch()) {
-    $reqs[] = new Requirement($fieldA, $fieldB);
-}
-
-$stmt->close();
-
 
 $mysqli->close();
 echo json_encode($combined);
