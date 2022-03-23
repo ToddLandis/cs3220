fc1782f07df7777028f4bcdcbb98278ff9f67cc4
Todd Landis <73204491+ToddLandis@users.noreply.github.com>
2022-03-22 20:50:49 -0400

    signup

--- Project4/signup.php
+++ Project4/signup.php
@@ -1,3 +1,99 @@
+<?php
+    $user   = htmlspecialchars($_POST["userfield"]);
+    $pass   = htmlspecialchars($_POST["passfield"]);
+    $cpass  = htmlspecialchars($_POST["confirmpassfield"]);
+    $name   = htmlspecialchars($_POST["namefield"]);
+    $plan   = htmlspecialchars($_POST["plannamefield"]);
+    $major  = htmlspecialchars($_POST["major"]);
+    $mode    = htmlspecialchars($_POST["mode"]);
+    
+    // CURRENT SEMESTER: TO BE UPDATED EACH REGISTRATION PERIOD
+    $cat_ID = 1;
+    $year = 2022;
+    $term = Spring;
+
+    if ($mode == 'dark') {
+        $mode = true;
+    }
+    else {
+        $mode = false;
+    }
+
+    // Check that username isn't taken
+    $mysqli = new mysqli('james', 'cs3220', '', 'cs3220_Sp22') 
+        or die('Database connect error.');
+    $stmt = $mysqli->prepare("SELECT ID FROM CHL_User WHERE Name = ?")
+        or die("Prepare error.");
+    $stmt->bind_param("s", $user)
+            or die('Database bind error.');
+
+    $stmt->execute()
+            or die('Database execute error.');
+    $stmt->store_result();  // optional for efficiency; fetches all results
+    $stmt->fetch();
+    if(mysql_num_rows($stmt) == 0) {
+        $taken = false;
+    }
+    else {
+        $taken = true;
+    }
+    $stmt->close();
+
+    if(taken) {
+        echo "Username is taken, please select a different one.";
+    }
+    else if (empty($user) || empty($pass) ||empty($cpass) ||empty($name) ||empty($plan) || empty($major)) || empty($major)){
+        echo "Please fill in every box.";
+    }
+    else if ($pass == $cpass) {
+        echo "Your passwords do not match";
+    }
+    else {
+        $mysqli = new mysqli('james', 'cs3220', '', 'cs3220_Sp22') 
+        or die('Database connect error.');
+        $stmt = $mysqli->prepare("INSERT INTO `CHL_User` (`ID`, `Name`, `Login`, `Password`, `Dark_Mode`) 
+            VALUES (NULL, ?, ?, ?, ?);
+            SELECT ID FROM CHL_User WHERE Name = ?")
+            or die("Prepare error.");
+        $stmt->bind_param("sssbs", $name, $user, $password, $mode, $user)
+                or die('Database bind error.');
+
+        $stmt->execute()
+                or die('Database execute error.');
+        $stmt->store_result();  // optional for efficiency; fetches all results
+        $stmt->bind_result($ID);// probably wrong, possibly more return values.
+        $stmt->fetch();
+        $stmt->close();
+
+
+        $stmt = $mysqli->prepare("INSERT INTO `CHL_Major` (`ID`, `User_ID`, `Major`) VALUES (NULL, ?, ?)
+        ")
+            or die("Prepare error.");
+        $stmt->bind_param("ss", $ID, $major)
+                or die('Database bind error.');
+        
+        $stmt->execute()
+                or die('Database execute error.');
+        $stmt->store_result();    // optional for efficiency; fetches all results
+        $stmt->bind_result($ID, $fieldB, $fieldC, $Dark_Mode);
+        $stmt->fetch();
+        $stmt->close();
+
+
+        $stmt = $mysqli->prepare("INSERT 
+            INTO `CHL_Plan` (`ID`, `User_ID`, `Catalog_ID`, `Plan_name`, `currYear`, `currTerm`, `Major`) 
+            VALUES (NULL, ?, ?, ?, ?, ?, ?)
+        ")
+            or die("Prepare error.");
+        $stmt->bind_param("sssiis", $ID, $cat_ID, $plan, $year, $term, $major)
+                or die('Database bind error.');
+        
+        $stmt->execute()
+                or die('Database execute error.');
+        $stmt->close();
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
