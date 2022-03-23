d47930fee64c75ed2f6a740a8c012cbded38727f
Todd Landis <73204491+ToddLandis@users.noreply.github.com>
2022-03-23 00:01:53 -0400

    getAll.php session

--- Project4/getAll.php
+++ Project4/getAll.php
@@ -1,10 +1,11 @@
 <?php
+
 $mysqli = new mysqli('james', 'cs3220', '', 'cs3220_Sp22') 
     or die('Database connect error.');
 
 // Arguments
-//session_start(); // This will directly bring in variables referenced like "$ID"
-$ID = 1; //DEBUG
+session_start(); // This will directly bring in variables referenced like "$ID"
+//$ID = 1; //DEBUG
 //$password = htmlspecialchars($_GET["pass"]); //FIXME session key
 
 // Return array
