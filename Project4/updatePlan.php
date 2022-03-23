6b5456f30cbd8a91e7e0cdf636c5423178d1b9c3
Todd Landis <73204491+ToddLandis@users.noreply.github.com>
2022-03-23 19:09:41 -0400

    finish updatePlan

--- Project4/updatePlan.php
+++ Project4/updatePlan.php
@@ -11,23 +11,49 @@ $mysqli = new mysqli('james', 'cs3220', '', 'cs3220_Sp22')
 for (i in $data) {
     //FIXME
     $change = $data[i].change;
-    $course = $data[i].course;
-    $term   = $data[i].term;
+    $plan   = $data[i].plan;    //TODO Make sure this is Plan_ID or use SQL to convert
+    $course = $data[i].course;  //TODO Make sure 
     $year   = $data[i].year;
+    $term   = $data[i].term;
+
+    // Convert Plan_name to Plan_ID
+    $stmt = $mysqli->prepare("SELECT `ID` from `CHL_Plan` 
+        where `User_ID` = ? AND `plan_name` = ?")
+        or die("Prepare error.");
+    $stmt->bind_param("ss", $ID, $plan)
+            or die('Database bind error.');
+    $stmt->execute()
+            or die('Database execute error.');
+    $stmt->store_result();
+    $stmt->bind_result($plan);
+    $stmt->fetch();
+    $stmt->close();
+    // Convert Course_Designator to Course_ID
+    $stmt = $mysqli->prepare("SELECT `ID` from `CHL_Course` 
+        where `Designator` = ?")
+        or die("Prepare error.");
+    $stmt->bind_param("i", $course)
+            or die('Database bind error.');
+    $stmt->execute()
+            or die('Database execute error.');
+    $stmt->store_result();
+    $stmt->bind_result($course);
+    $stmt->fetch();
+    $stmt->close();
 
     if ($change = 'insert') {
-    $stmt = $mysqli->prepare("INSERT INTO `Plan_Courses` (`ID`, `Name`, `Login`, `Password`, `Dark_Mode`) VALUES (NULL, ?, ?, ?, ?);")
+    $stmt = $mysqli->prepare("INSERT INTO `Plan_Courses` (`ID`, `Plan_ID`, `Course_ID`, `Year`, `Semester`) VALUES (NULL, ?, ?, ?, ?);")
             or die("Prepare error.");
-    $stmt->bind_param("ssss", $name, $user, $pass, $mode)
+    $stmt->bind_param("ssss", $plan, $course, $year, $term)
             or die('Database bind error.');
     $stmt->execute();
         
     $stmt->close();
     }
     else if ($change = 'delete') {
-        $stmt = $mysqli->prepare("DELETE FROM `CHL_User` (`ID`, `Name`, `Login`, `Password`, `Dark_Mode`) VALUES (NULL, ?, ?, ?, ?);")
+        $stmt = $mysqli->prepare("DELETE FROM `CHL_Plan_Courses` WHERE `CHL_Plan_Courses`.`Plan_ID` = ? AND `CHL_Plan_Courses`.`Course_ID` = ?")
             or die("Prepare error.");
-        $stmt->bind_param("ssss", $name, $user, $pass, $mode)
+        $stmt->bind_param("ss", $plan, $course)
                 or die('Database bind error.');
         $stmt->execute();
             
