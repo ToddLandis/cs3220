ccc2f44351601955a6f449f812277f727d6a9bce
Todd Landis <73204491+ToddLandis@users.noreply.github.com>
2022-03-23 20:34:40 -0400

    debug

--- Project4/dragndrop.js
+++ Project4/dragndrop.js
@@ -163,6 +163,7 @@ function allowDrop(ev) {
 // write modified plan to the database
 function savePlan() {
  var dataString = JSON.stringify(changestack);
+ console.log("JSON Data: " + dataString);
  $.ajax({
     url: 'updatePlan.php',
     data: {myData: dataString},
