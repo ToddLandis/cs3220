dd93f88a54b2636e12f24c36a09a147e888739de
Todd Landis <73204491+ToddLandis@users.noreply.github.com>
2022-04-28 14:53:30 -0400

    Create userInfo.php

---
+++ Project6/userInfo.php
@@ -0,0 +1,26 @@
+<?php
+    $name = htmlspecialchars($_POST["userfield"]);
+    $pass = htmlspecialchars($_POST["passfield"]);
+    $return = array();
+    $mysqli = new mysqli('james', 'cs3220', '', 'cs3220_Sp22') 
+    or die('Database connect error.');
+    $stmt = $mysqli->prepare("SELECT ID, Name, Password, Dark_Mode FROM CHL_User 
+        WHERE Login = ?")
+        or die("Prepare error.");
+    $stmt->bind_param("s", $name)
+            or die('Database bind error.');
+
+    $stmt->execute()
+            or die('Database execute error.');
+    $stmt->store_result();    // optional for efficiency; fetches all results
+    $stmt->bind_result($ID, $Name, $Password, $Dark_Mode);
+    $stmt->fetch();
+    $return['ID'] = $ID;
+    $return['Username'] = $Name;
+    $return['Password'] = $Password;
+    $return['Dark_Mode'] = $Dark_Mode;
+    $stmt->close();
+    $mysqli->close();
+    
+    echo json_encode();
+?>
