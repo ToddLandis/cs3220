f77d0e27d7a3830b3f881d5312aa4d76e1a31bbf
Todd Landis <73204491+ToddLandis@users.noreply.github.com>
2022-04-28 18:24:29 -0400

    Update userInfo.php

--- Project6/userInfo.php
+++ Project6/userInfo.php
@@ -5,15 +5,15 @@
     $return = array();
 
     $mysqli = new mysqli('james', 'cs3220', '', 'cs3220_Sp22') 
-    or die('Database connect error.');
+    or (die('Database connect error.') and echo("Database connect error."));
     $stmt = $mysqli->prepare("SELECT ID, Name, Password, Dark_Mode FROM CHL_User 
         WHERE Login = ?")
-        or die("Prepare error.");
+        or (die("Prepare error.") and echo("Prepare Error.");
     $stmt->bind_param("s", $name)
-            or die('Database bind error.');
+            or (die('Database bind error.') and echo("Database bind error."));
 
     $stmt->execute()
-            or die('Database execute error.');
+            or (die('Database execute error.') and echo("Database execute error"));
     $stmt->store_result();    // optional for efficiency; fetches all results
     $stmt->bind_result($ID, $Name, $Password, $Dark_Mode);
     $stmt->fetch();
