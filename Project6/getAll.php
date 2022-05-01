b26bc923d2ac9f0e6e11066bd4543ff5da67685f
Todd Landis <73204491+ToddLandis@users.noreply.github.com>
2022-04-30 21:29:00 -0400

    getAll.php

--- Project6/getAll.php
+++ Project6/getAll.php
@@ -74,7 +74,12 @@ for ($i = 0; $i < count($plans); $i++) {
         $plan_courses = array();
         while ($stmt->fetch()) {
                 //array_push($plan_courses, ['DBID'=>$fieldA, 'id'=>$fieldB, 'name'=>$fieldC, 'year'=>$fieldD, 'term'=>$fieldE]);
-                $plan_courses[$fieldB] = ['DBID'=>$fieldA, 'id'=>$fieldB, 'name'=>$fieldC, 'year'=>$fieldD, 'term'=>$fieldE];
+                if ($fieldE == "Fall") {
+                        $plan_courses[$fieldB] = ['DBID'=>$fieldA, 'id'=>$fieldB, 'name'=>$fieldC, 'year'=>$fieldD, 'term'=>$fieldE];
+                }
+                else {
+                        $plan_courses[$fieldB] = ['DBID'=>$fieldA, 'id'=>$fieldB, 'name'=>$fieldC, 'year'=>$fieldD-1, 'term'=>$fieldE];
+                }
         }
         $plans[$i]['courses'] = $plan_courses;
         $stmt->close();
